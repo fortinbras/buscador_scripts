cded009ca4c259e737741f003a96652e37b01dd3
giuseppe <giusepper11@gmail.com>
2018-06-12 09:20:38 -0300

    add inep

---
+++ Transform/inep_download/inep_download.py
@@ -0,0 +1,77 @@
+# -*- coding: utf-8 -*-
+
+import os, errno
+import requests
+import zipfile
+import sys
+
+"""
+Este script faz o downloa dos arquivos do INEP e descompacta-os na pasta dos respectivos anos.
+Deve-se escolher o ano de inicio e o ano final dos arquivos a serem baixados.
+Antes de 2009, a nomenclatura e formato dos arquivos nao batem.
+"""
+
+all_links = {
+    '1995': 'http://download.inep.gov.br/microdados/micro_censo_edu_superior1995.zip',
+    '1996': 'http://download.inep.gov.br/microdados/micro_censo_edu_superior1996.zip',
+    '1997': 'http://download.inep.gov.br/microdados/micro_censo_edu_superior1997.zip',
+    '1998': 'http://download.inep.gov.br/microdados/micro_censo_edu_superior1998.zip',
+    '1999': 'http://download.inep.gov.br/microdados/micro_censo_edu_superior1999.zip',
+    '2000': 'http://download.inep.gov.br/microdados/micro_censo_edu_superior2000.zip',
+    '2001': 'http://download.inep.gov.br/microdados/micro_censo_edu_superior2001.zip',
+    '2002': 'http://download.inep.gov.br/microdados/micro_censo_edu_superior2002.zip',
+    '2003': 'http://download.inep.gov.br/microdados/micro_censo_edu_superior2003.zip',
+    '2004': 'http://download.inep.gov.br/microdados/microdados_censo_educacao_superior_2004.zip',
+    '2005': 'http://download.inep.gov.br/microdados/microdados_censo_educacao_superior_2005.zip',
+    '2006': 'http://download.inep.gov.br/microdados/microdados_educacao_superior_2006.zip',
+    '2007': 'http://download.inep.gov.br/microdados/microdados_educacao_superior_2007.zip',
+    '2008': 'http://download.inep.gov.br/microdados/micro_censo_edu_superior2008.zip',
+    '2009': 'http://download.inep.gov.br/microdados/microdados_censo_superior_2009.zip',
+    '2010': 'http://download.inep.gov.br/microdados/microdados_censo_superior_2010.zip',
+    '2011': 'http://download.inep.gov.br/microdados/microdados_censo_superior_2011.zip',
+    '2012': 'http://download.inep.gov.br/microdados/microdados_censo_superior_2012.zip',
+    '2013': 'http://download.inep.gov.br/microdados/microdados_censo_superior_2013.zip',
+    '2014': 'http://download.inep.gov.br/microdados/microdados_censo_superior_2014.zip',
+    '2015': 'http://download.inep.gov.br/microdados/microdados_censo_superior_2015.zip',
+    '2016': 'http://download.inep.gov.br/microdados/microdados_censo_superior_2016.zip'
+}
+
+
+def download_censo_superior(ano):
+    dir_destino = '/var/tmp/inep/docentes/arquivos/'
+    if isinstance(ano,str):
+        ano_str = str(ano)
+        nome_arquivo = ano_str + '.zip'
+
+        # Fazendo o download do arquivo
+        url = all_links[ano_str]
+        print(url)
+        resp = requests.get(url)
+
+        # Criando o diretorio de destino dos arquivos baixados
+        try:
+            os.makedirs(dir_destino)
+        except OSError as e:
+            if e.errno != errno.EEXIST:
+                raise
+
+        # Gravando o arquivo zip
+        with open(nome_arquivo, 'wb') as f:
+            f.write(resp.content)
+
+        # Descompactando o arquivo zip
+        zip_ref = zipfile.ZipFile(nome_arquivo, 'r')
+        zip_ref.extractall(dir_destino)
+        zip_ref.close()
+
+        # Apagando o arquivo zip
+        os.remove(nome_arquivo)
+
+
+if __name__ == "__main__":
+    try:
+        download_censo_superior(sys.argv[1])
+    except IndexError:
+        print('digite ano que deseja como parametro entre (1995~2010)')
+    except KeyError:
+        print('Ano invalido digite ano entre (1995~2010)')
