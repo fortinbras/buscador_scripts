dae98894c5ef07ae6af0fb56262bde298907a5e5
DevGra <carlosgraziani.toledo@gmail.com>
2018-10-23 13:54:59 -0300

    fix na transformacao dos dicionarios, todos os anos estao ok

--- _transform/rais_estabelecimento/rais_estabelecimento.py
+++ _transform/rais_estabelecimento/rais_estabelecimento.py
@@ -2,6 +2,8 @@
 import errno
 import os
 import sys
+import unicodedata
+import re
 
 sys.path.insert(0, os.path.abspath('.'))
 sys.path.insert(0, '../../../buscador_scripts/')
@@ -36,19 +38,6 @@ class RaisEstabelecimentoTransform(object):
         self.df = pd.DataFrame()
         self.f = ''
         self.ano = ano
-        # planilha tem 24 campos
-        #self.colunas = [
-
-            #u'CNAE 2.0 Classe', u'CNAE 95 Classe',
-            #u'Qtd Vínculos CLT', u'Qtd Vínculos Ativos',
-            #u'Qtd Vínculos Estatutários', u'Ind Atividade Ano',
-            #u'Ind CEI Vinculado', u'Ind Estab Participa PAT',
-            #u'Ind Rais Negativa', u'Ind Simples', u'Município',
-            #u'Natureza Jurídica', u'Regiões Adm DF',
-            #u'CNAE 2.0 Subclasse', u'Tamanho Estabelecimento',
-            #u'Tipo Estab', u'Tipo Estab', u'UF',u'IBGE Subsetor', u'CEP Estb',
-
-        #]
         self.variaveis = {
 
             u'CNAE 95 Classe': cnae1_dic(),
@@ -81,15 +70,13 @@ class RaisEstabelecimentoTransform(object):
                     arquivo = open(os.path.join(root, f), 'r')
                     self.input_lenght = commands.getstatusoutput('cat ' + os.path.join(root, f) + ' |wc -l')[1]
                     print 'Arquivo {} de entrada possui {} linhas de informacao'.format(f, int(self.input_lenght))
-                    #iterdf = pd.read_csv(arquivo, sep=';', chunksize=200000, encoding='latin-1', low_memory=False)
-                    iterdf = pd.read_csv(arquivo, sep=';', nrows=1000, chunksize=500, encoding='latin-1', low_memory=False)
+                    iterdf = pd.read_csv(arquivo, sep=';', chunksize=200000, encoding='latin-1', low_memory=False)
+                    #iterdf = pd.read_csv(arquivo, sep=';', nrows=1000, chunksize=500, encoding='latin-1', low_memory=False)
                     self.c = 0
 
                     for df in iterdf:
                         nfile = self.destino_transform + self.f + '_' + str(self.c)+ '.csv'
                         df = self.resolve_dicionario(df)
-                        #import pdb; pdb.set_trace()
-
                         try:
                             os.makedirs(self.destino_transform)
                         except OSError as e:
@@ -114,78 +101,31 @@ class RaisEstabelecimentoTransform(object):
                     # os.remove(os.path.join(root, f))
 
     def resolve_dicionario(self, df):
-
+        # import pdb; pdb.set_trace()
         for item in self.avoid:
             del (df[item])
-        # import pdb; pdb.set_trace()
-        # if self.ano == '2013':
-        #     for item in self.variaveis:
-        #         if item == 'IBGE Subsetor':
-        #             print item
-        #             df.drop(df.columns[[item]], axis=1, inplace=True)
-                    #df.drop('item', axis=1, inplace=True)
-        # if self.ano in '2010 2011 2012':
-        #     for item in self.avoid_anteriores:
-        #         del (df[item])
+        #A coluna UF não existe nos arquivos(anos:2010, 2011, 2012), portanto
+        #foi feito slice do index de municipio e passado o codigo do Mestrado
+        #para o campo UF criado.
+        codigo = df[u'Município'].apply(str)
+        df[u'UF'] = codigo.str.slice(0,2).astype(int)
 
-
-
-        #df.columns = df.columns.str.replace(' ', '_')
-
-        #import pdb; pdb.set_trace()
-        #
-        # for index, row in df.iterrows():
-        #     print '"%s"' %row['Bairros SP']
-        #     if row['Bairros SP'] == u'            {Ã± class}':
-        #         row['Bairros SP'] = 'Ignorado'
-        #     print row['Bairros SP']
-
-        #df.columns = [remover_acentos(df) for x in list(df.columns)]
-        #df['ID'] = [self.f + '_' + str(self.c) + '_' + str(i + 1) for i in range(df.index.size)]
-        #import pdb; pdb.set_trace()
-        # df[df[u'Bairros SP'].str.contains(u'{Ã± class}')][u'Bairros SP'] = ''
-        # df[df[u'Bairros SP'] == u'{Ã± class}'][u'Bairros SP'] = ''
-        #print df[u'Bairros SP']
-
-        # import pdb; pdb.set_trace()
-        #df.columns = [remover_acentos(x) for x in list(df.columns)]
         for k, v in self.variaveis.items():
-
+            # import pdb; pdb.set_trace()
             try:
-                # import pdb;pdb.set_trace()
-                #df[k] = df[k].astype('int')
-
-
-                import pdb; pdb.set_trace()
-                if self.ano in '201020112012':
-                    for col in df.columns:
-                        if col == 'IBGE Subsetor':
-                            print col
-                            df.drop(df.columns[[-2]], axis=1, inplace=True) # deleta a ultima coluna que esta sobrando e sem valor
-                    print('aqui')
-                    df[u'UF'] = df[u'Município'].str.split('-').str.get(0)[:3]
-
                 df[k] = df[k].map(v).fillna(df[k])
 
-
-                # import pdb; pdb.set_trace()
-                # print 'DF[k]..................................'
-                # print ('K {} '.format(k))
-                # print 'DF[k]..................................'
-                #exit()
             except KeyError:
                 df[k] = 'Não disponivel'
-
-        if self.ano == '2013': #para não exibir a coluna vazia IBGE setor, que não está no ESTB2013.txt
+        #para não exibir a coluna vazia IBGE setor, que não está no ESTB2013.txt
+        if self.ano in ['2010','2011','2012','2013']:
             for col in df.columns:
                 if col == 'IBGE Subsetor':
-                    print col
-                    df.drop(df.columns[[-1]], axis=1, inplace=True) # deleta a ultima coluna que esta sobrando e sem valor
+                    del(df[u'IBGE Subsetor'])
 
         df[u'Município'] = df[u'Município'].str.split('-').str.get(1)
-
-        import pdb; pdb.set_trace()
-
+        #df.columns = df.columns.str.replace(' ', '_')
+        #df.columns = [remover_acentos(x) for x in list(df.columns)]
         return df
 
 
@@ -197,15 +137,12 @@ def rais_estabelecimento_transform():
         print anos
     except:
         raise
-    rais_estabelecimento = RaisEstabelecimentoTransform('2011')
-    rais_estabelecimento.pega_arquivos_ano()
+    #rais_estabelecimento = RaisEstabelecimentoTransform('2010')
+    #rais_estabelecimento.pega_arquivos_ano()
 
-    # for ano in anos:
-    #     rais = RaisEstabelecimentoTransform(ano)
-    #     rais.pega_arquivos_ano()
-
-import unicodedata
-import re
+    for ano in anos:
+        rais = RaisEstabelecimentoTransform(ano)
+        rais.pega_arquivos_ano()
 
 def remover_acentos(palavra, codif='utf-8'):
 
@@ -213,56 +150,3 @@ def remover_acentos(palavra, codif='utf-8'):
     palavraSemAcento = u"".join([c for c in nfkd if not unicodedata.combining(c.decode(codif))])
     palavraSemAcento =  re.sub('[^a-zA-Z0-9 \\\]', '', palavraSemAcento)
     return palavraSemAcento.replace(' ','_').replace('__','_').lower()
-
-'''
-def eliminar_acentos(palavra):
-
-    d = {
-        '\xc1':'A',
-        '\xc9':'E',
-        '\xcd':'I',
-        '\xd3':'O',
-        '\xda':'U',
-        '\xdc':'U',
-        '\xd1':'N',
-        '\xc7':'C',
-        '\xed':'i',
-        '\xf3':'o',
-        '\xf1':'n',
-        '\xe7':'c',
-        '\xba':'',
-        '\xb0':'',
-        '\x3a':'',
-        '\xe1':'a',
-        '\xe2':'a',
-        '\xe3':'a',
-        '\xe4':'a',
-        '\xe5':'a',
-        '\xe8':'e',
-        '\xe9':'e',
-        '\xea':'e',
-        '\xeb':'e',
-        '\xec':'i',
-        '\xed':'i',
-        '\xee':'i',
-        '\xef':'i',
-        '\xf2':'o',
-        '\xf3':'o',
-        '\xf4':'o',
-        '\xf5':'o',
-        '\xf0':'o',
-        '\xf9':'u',
-        '\xfa':'u',
-        '\xfb':'u',
-        '\xfc':'u',
-        '\xe5':'a'
-    }
-
-    nueva_palavra = palavra
-    for c in d.keys():
-        nueva_palavra = nueva_palavra.replace(c,d[c])
-
-    auxiliar = nueva_palavra.encode('latin-1')
-    #auxiliar = nueva_palavra.encode('utf-8')
-    return nueva_palavra
-'''
